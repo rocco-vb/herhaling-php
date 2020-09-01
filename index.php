92f2158f4750dc5a68ddf6007abbee7b4bef5643
rocco <roccomedianl@gmail.com>
2020-09-01 16:11:46 +0200

    Ik heb de oefeningen van 2 – Herhaling – Oefeningen gemaakt.

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+//Variables
+$getal = 1;
+
+echo is_int($getal);
+
+PHP_EOL;
+
+//Array + for-loop
+
+$klas = ["Piet van Dijk", "Rembrandt van Rijn", "Vincent van Gogh", "Floris van Dyck"]; 
+
+for($i = 0; $i<count($klas); $i++){
+    echo $klas[$i], PHP_EOL;
+}
+
+PHP_EOL;
+
+foreach($klas as $item){
+    echo $item, PHP_EOL;
+}
+
+PHP_EOL;
+
+//Combinatie Opdracht
+
+$activiteiten = readline("Hoeveel activiteiten wil je toevoegen aan je bucket list?");
+
+for($i=0; $i<$activiteiten; $i++){
+    $welkeArray[] = readline("Welke activiteiten wil je toevoegen?");
+}
+
+print_r($welkeArray);
+
+foreach($welkeArray as $item){
+    echo $item, PHP_EOL;
+}
