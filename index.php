38ee962624608a9d872d502b7511d179ec226b6f
rocco <roccomedianl@gmail.com>
2020-09-02 11:39:15 +0200

    Ik heb de oefeningen van 2 – Herhaling – Oefeningen nogmaals wat opgeschoond en onnodige code weggehaald.

--- index.php
+++ index.php
@@ -5,35 +5,31 @@ $getal = 1;
 
 echo is_int($getal);
 
-PHP_EOL;
-
 //Array + for-loop
 
 $klas = ["Piet van Dijk", "Rembrandt van Rijn", "Vincent van Gogh", "Floris van Dyck"]; 
+
+print_r($klas);
 
 for($i = 0; $i<count($klas); $i++){
     echo $klas[$i], PHP_EOL;
 }
 
-PHP_EOL;
-
 foreach($klas as $item){
     echo $item, PHP_EOL;
 }
 
-PHP_EOL;
-
 //Combinatie Opdracht
 
-$activiteiten = readline("Hoeveel activiteiten wil je toevoegen aan je bucket list?");
-$welkeArray = array();
+$hoeveelheid_activiteiten = readline("Hoeveel activiteiten wil je toevoegen aan je bucket list?");
+$bucket_list = array();
 
-for($i=0; $i<$activiteiten; $i++){
-    array_push($welkeArray, readline("Welke activiteiten wil je toevoegen?"));
+for($i=0; $i<$hoeveelheid_activiteiten; $i++){
+    array_push($bucket_list, readline("Welke activiteiten wil je toevoegen?"));
 }
 
-print_r($welkeArray);
+print_r($bucket_list);
 
-foreach($welkeArray as $item){
+foreach($bucket_list as $item){
     echo $item, PHP_EOL;
 }
